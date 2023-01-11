2e6117d6ee33893e53803c1488763b0221f5854a
susana <susalbert1@hotmail.com>
2023-01-11 13:17:47 +0100

    cambios jv pruebas

--- pruebasjs.js
+++ pruebasjs.js
@@ -7,4 +7,15 @@ if (saludo) {
 function multiply(a, b){
     return a * b; 
   }
+  let isDigit = 3;
+
+  if (isDigit < 10 || isDigit> 18) {
+    alert( 'es verdadero' );
+  }
+   let Digit = 3-4;
+
+if (Digit < 1 || Digit> 8) {
+  alert( 'es falso' );
+}
+
   
