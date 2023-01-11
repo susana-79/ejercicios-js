6f7e537dc3bb30349bc281c0f0b481eca89159a2
susana <susalbert1@hotmail.com>
2023-01-11 12:47:09 +0100

    cambios en java

---
+++ pruebasjs.js
@@ -0,0 +1,10 @@
+let saludo;
+saludo = prompt("Pon tu nombre")
+console.log(saludo)
+if (saludo) {
+    console.log("Hola,¿como estas hoy?")
+}
+function multiply(a, b){
+    return a * b; 
+  }
+  
