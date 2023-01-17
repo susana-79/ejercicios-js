fea964432f8a27ecf02477e84f6ab6d5254cb551
susana <susalbert1@hotmail.com>
2023-01-17 22:24:07 +0100

    cambios en java

--- array.js
+++ array.js
@@ -3,26 +3,33 @@
 
 //Iterar por todos los elementos dentro de un array utilizando while 
 //y mostrarlos en pantalla.
-let numeros = [1, 2, 3, 4, 5, 6];
-while (numeros <=numeros. length) {
-    console.log(numeros)
-    cont++
+let numeros = [1,2,3,4,5,6];
+let i= 0
+while (i <=numeros.length) {
+    console.log(numeros[i])
+    i++
 }
    
 
 //Iterar por todos los elementos dentro de un array
 // utilizando for y mostrarlos en pantalla.
- numeros = [1, 2, 3, 4, 5, 6];
-function mostrarNumeros(numeros) {
+numeros = [1,2,3,4,5,6];
 
-    //Mostrar todos los elementos dentro de un array sumándole uno a cada uno.
-    this.numeros++;
+
     for (let i = 0; i <= numeros.length; i++){
         console.log(numeros[i])
     }
-}
 
+//Mostrar todos los elementos dentro de un array sumándole uno a cada uno.
+numeros=[1,2,3,4,5,6];
+let o=1
+for (let o=1; o+numeros;o++){
+    console.log(numeros[o])
+}
 
 //Generar una copia de un array pero con todos los elementos incrementado en 1.
 //Calcular la media de la suma total de sus elementos 
 
+//2. Pediremos a través de un bluce 6 números al usuario que almacenaremos en un array
+//Imprimeros por pantalla la longitud del array, el número más alto y el número más bajo
+//Objetos:
