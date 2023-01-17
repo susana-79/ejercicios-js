43f1ae27774bf5c0e102336cf9eb00af866f5589
susana <susalbert1@hotmail.com>
2023-01-17 21:39:11 +0100

    cambios

--- array.js
+++ array.js
@@ -3,18 +3,26 @@
 
 //Iterar por todos los elementos dentro de un array utilizando while 
 //y mostrarlos en pantalla.
-
+let numeros = [1, 2, 3, 4, 5, 6];
+while (numeros <=numeros. length) {
+    console.log(numeros)
+    cont++
+}
+   
 
 //Iterar por todos los elementos dentro de un array
 // utilizando for y mostrarlos en pantalla.
-let numeros = [1, 2, 3, 4, 5, 6];
+ numeros = [1, 2, 3, 4, 5, 6];
 function mostrarNumeros(numeros) {
+
+    //Mostrar todos los elementos dentro de un array sumándole uno a cada uno.
+    this.numeros++;
     for (let i = 0; i <= numeros.length; i++){
         console.log(numeros[i])
     }
 }
-//Mostrar todos los elementos dentro de un array sumándole uno a cada uno.
-numeros = (1, 2, 3, 4, 5, 6);
+
+
 //Generar una copia de un array pero con todos los elementos incrementado en 1.
 //Calcular la media de la suma total de sus elementos 
 
